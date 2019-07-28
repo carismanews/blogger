3ba7668e7c37316dd57b87898f2166bdc711482c
carismanews <50420028+carismanews@users.noreply.github.com>
2019-07-27 21:22:59 -0300

    Create nightmode.js

---
+++ nightmode.js
@@ -0,0 +1,16 @@
+//night mode
+if(localStorage.getItem("darkmode")){
+  document.querySelector("html").classList.toggle("noite");
+  // document.querySelector(".fas").classList.toggle("fa-sun");
+}
+function toggleMode(){
+  var ary = document.querySelector("html").classList;
+  document.querySelector("html").classList.toggle("noite");
+  if(Object.values(ary).includes("noite")){
+    // document.querySelector(".fas").classList.toggle("fa-sun");
+    localStorage.setItem("darkmode", "1");  
+  }else{
+    // document.querySelector(".fas").classList.toggle("fa-sun");
+    localStorage.removeItem("darkmode");  
+  }
+}
