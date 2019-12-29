4da1e4f340423fe71812c672d2d3da39f7b500ac
carismanews <50420028+carismanews@users.noreply.github.com>
2019-12-28 23:16:24 -0300

    Update meuscript.js

--- meuscript.js
+++ meuscript.js
@@ -1,1 +1,1 @@
-$(function(){$(window).scroll(function(){$(this).scrollTop()>80?$(".head-wrap").addClass("fixed"):$(".head-wrap").removeClass("fixed")})});
+$(function(){$(window).scroll(function(){$(this).scrollTop()>80?$(".fixed-header").addClass("fixed"):$(".fixed-header").removeClass("fixed")})});
